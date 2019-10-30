9c3b2c0cbcb43dfefa40bf2b156dbca40b22c00e
Zoe Gadon-Thompson <zoe.gadonthompson@farsetlabs.org.uk>
2019-10-30 17:58:46 +0000

    fixed name errors

--- tests/test_dtypes.py
+++ tests/test_dtypes.py
@@ -3,6 +3,9 @@ import moneypandas
 from moneypandas import parser, MoneyArray, dtypes
 
 def test_find_currency_data():
-    expected_output = dtypes.find_currency_data()
-    result = {'＄': 'ARS', '﹩': 'ARS', '$': 'ARS', '￡': 'FKP', '₭': 'LAK', '£': 'FKP', '￦': 'KPW', '￥': 'CNY', '﷼': 'IRR', '₾': 'GEL', '₽': 'RUB', '₼': 'AZN', '₺': 'TRY', '₹': 'INR', '₸': 'KZT', '₵': 'GHS', '₴': 'UAH', '₲': 'PYG', '₱': 'CUP', '₮': 'MNT', '€': 'EUR', '₫': 'VND', '₪': 'ILS', '₩': 'KPW', '₨': 'LKR', '₦': 'NGN', '₡': 'CRC', '៛': 'KHR', '฿': 'THB', '৳': 'BDT', '؋': 'AFN', '֏': 'AMD', '¥': 'CNY', '¢': 'GHS'}
-    assert result == expected_output
+    result = dtypes.find_currency_data()
+    expected = {'＄': 'ARS', '﹩': 'ARS', '$': 'ARS', '￡': 'FKP', '₭': 'LAK', '£': 'FKP', '￦': 'KPW', '￥': 'CNY', '﷼': 'IRR', '₾': 'GEL', '₽': 'RUB', '₼': 'AZN', '₺': 'TRY', '₹': 'INR', '₸': 'KZT', '₵': 'GHS', '₴': 'UAH', '₲': 'PYG', '₱': 'CUP', '₮': 'MNT', '€': 'EUR', '₫': 'VND', '₪': 'ILS', '₩': 'KPW', '₨': 'LKR', '₦': 'NGN', '₡': 'CRC', '៛': 'KHR', '฿': 'THB', '৳': 'BDT', '؋': 'AFN', '֏': 'AMD', '¥': 'CNY', '¢': 'GHS'}
+    assert result == expected
+
+def test_find_currency_data_type():
+    expected_result = 
