41197e1e0cda226c5584715c2827c1ea1b4c7e0f
Zoe Gadon-Thompson <zoe.gadonthompson@farsetlabs.org.uk>
2019-10-30 09:59:39 +0000

    test_parser file linted

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,7 +1,10 @@
-import money
-
+# pylint: disable=protected-access, missing-function-docstring, fixme
+"""
+Unit tests for parser.
+"""
 import pytest
-
+from iso4217parse import Currency
+import money
 from moneypandas import parser, MoneyArray
 
 
@@ -9,7 +12,7 @@ from moneypandas import parser, MoneyArray
     [u'123 EUR',
      u'234 EUR']
     # TODO: reinstate byte tests
-    #[b'\xc0\xa8\x01\x01',
+    # [b'\xc0\xa8\x01\x01',
     # b' \x01\r\xb8\x85\xa3\x00\x00\x00\x00\x8a.\x03ps4'],
 ])
 def test_to_money(values):
@@ -37,3 +40,20 @@ def test_as_money_object(val, expected, money_code):
 def test_as_money_object_raises(val):
     with pytest.raises(ValueError):
         parser._as_money_object(val)
+
+
+def test__currency_symbol():
+    """ testing the value returned from _currency_symbol """
+    expected = [Currency(
+        alpha3='USD',
+        code_num=840,
+        name='United States dollar',
+        symbols=['US$', '$', '＄', '﹩', 'dollar',
+                 'dollars', 'Dollar', 'Dollars', 'US＄', 'US﹩'],
+        minor=2,
+        countries=['AS', 'BB', 'BM', 'BQ', 'EC', 'FM', 'GU', 'HT', 'IO', 'MH',
+                   'MP', 'PA', 'PR', 'PW', 'SV', 'TC', 'TL', 'UM', 'US', 'VG', 'VI']
+    )]
+    test_value = 'USD 4'
+    result = parser._currency_symbol(test_value)
+    assert result == expected
