c9afc08b5ce95d1aa196ae8433caf62133a262fe
Zoe Gadon-Thompson <zoe.gadonthompson@farsetlabs.org.uk>
2019-11-04 16:31:58 +0000

    changing import for Decimal

--- examples/three_currency.py
+++ examples/three_currency.py
@@ -1,13 +1,13 @@
 from moneypandas import MoneyArray
 from money import xrates
-import decimal
+from decimal import Decimal
 import pandas as pd
 
 
 xrates.install('money.exchange.SimpleBackend')
 xrates.base = 'USD'
-xrates.setrate('EUR', decimal.Decimal('0.9'))
-xrates.setrate('GBP', decimal.Decimal('0.8'))
+xrates.setrate('EUR', Decimal(0.9))
+xrates.setrate('GBP', Decimal(0.8))
 
 df = pd.DataFrame({"money": MoneyArray(['1284 EUR', '121 EUR', '€14', '£12'], 'USD')})
 total = df['money'].sum()
